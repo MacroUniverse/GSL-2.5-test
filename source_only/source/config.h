06ad2c11e4850d99cc7cd2f7ba1ec16a4f353d12
MacroUniverse <270174408@qq.com>
2018-12-31 12:45:14 -0600

    g++ successful
    * use "Understand dependency graph" to write Makefile
    * "config.h" is generated by autotool for linx.phys.ksu.edu, different from Visual Studio
    * "coerce.c" is needed now due to conditional compilation

--- source_only/source/config.h
+++ source_only/source/config.h
@@ -1,10 +1,11 @@
+/* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Disable deprecated functions and enums while building */
 #define GSL_DISABLE_DEPRECATED 1
 
 /* Define if you have inline with C99 behavior */
-#undef HAVE_C99_INLINE
+/* #undef HAVE_C99_INLINE */
 
 /* Define to 1 if you have the declaration of `acosh', and to 0 if you don't.
    */
@@ -24,19 +25,19 @@
 
 /* Define to 1 if you have the declaration of `feenableexcept', and to 0 if
    you don't. */
-#undef HAVE_DECL_FEENABLEEXCEPT
+#define HAVE_DECL_FEENABLEEXCEPT 1
 
 /* Define to 1 if you have the declaration of `fesettrapenable', and to 0 if
    you don't. */
-#undef HAVE_DECL_FESETTRAPENABLE
+#define HAVE_DECL_FESETTRAPENABLE 0
 
 /* Define to 1 if you have the declaration of `finite', and to 0 if you don't.
    */
-#undef HAVE_DECL_FINITE
+#define HAVE_DECL_FINITE 1
 
 /* Define to 1 if you have the declaration of `fprnd_t', and to 0 if you
    don't. */
-#undef HAVE_DECL_FPRND_T
+#define HAVE_DECL_FPRND_T 0
 
 /* Define to 1 if you have the declaration of `frexp', and to 0 if you don't.
    */
@@ -67,37 +68,37 @@
 #define HAVE_DECL_LOG1P 1
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
-#undef HAVE_DLFCN_H
+#define HAVE_DLFCN_H 1
 
 /* Define to 1 if you don't have `vprintf' but do have `_doprnt.' */
-#undef HAVE_DOPRNT
+/* #undef HAVE_DOPRNT */
 
 /* Defined if you have ansi EXIT_SUCCESS and EXIT_FAILURE in stdlib.h */
 #define HAVE_EXIT_SUCCESS_AND_FAILURE 1
 
 /* Defined on architectures with excess floating-point precision */
-#undef HAVE_EXTENDED_PRECISION_REGISTERS
+#define HAVE_EXTENDED_PRECISION_REGISTERS 1
 
 /* Define if x86 processor has sse extensions. */
 #define HAVE_FPU_X86_SSE 1
 
 /* Define to 1 if you have the <ieeefp.h> header file. */
-#undef HAVE_IEEEFP_H
+/* #undef HAVE_IEEEFP_H */
 
 /* Define this if IEEE comparisons work correctly (e.g. NaN != NaN) */
-#undef HAVE_IEEE_COMPARISONS
+#define HAVE_IEEE_COMPARISONS 1
 
 /* Define this if IEEE denormalized numbers are available */
-#undef HAVE_IEEE_DENORMALS
+#define HAVE_IEEE_DENORMALS 1
 
 /* Define if you have inline */
-#define HAVE_INLINE
+#define HAVE_INLINE 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#undef HAVE_INTTYPES_H
+#define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the `m' library (-lm). */
-#undef HAVE_LIBM
+#define HAVE_LIBM 1
 
 /* Define to 1 if you have the `memcpy' function. */
 #define HAVE_MEMCPY 1
@@ -109,7 +110,7 @@
 #define HAVE_MEMORY_H 1
 
 /* Define this if printf can handle %Lf for long double */
-#undef HAVE_PRINTF_LONGDOUBLE
+#define HAVE_PRINTF_LONGDOUBLE 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
@@ -118,86 +119,86 @@
 #define HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the `strdup' function. */
-#undef HAVE_STRDUP
+#define HAVE_STRDUP 1
 
 /* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
+#define HAVE_STRINGS_H 1
 
 /* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
 
 /* Define to 1 if you have the `strtol' function. */
-#undef HAVE_STRTOL
+#define HAVE_STRTOL 1
 
 /* Define to 1 if you have the `strtoul' function. */
-#undef HAVE_STRTOUL
+#define HAVE_STRTOUL 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#undef HAVE_SYS_STAT_H
+#define HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#undef HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
+#define HAVE_UNISTD_H 1
 
 /* Define to 1 if you have the `vprintf' function. */
-#undef HAVE_VPRINTF
+#define HAVE_VPRINTF 1
 
 /* Define if you need to hide the static definitions of inline functions */
-#undef HIDE_INLINE_STATIC
+/* #undef HIDE_INLINE_STATIC */
 
 /* Define to the sub-directory in which libtool stores uninstalled libraries.
    */
-#undef LT_OBJDIR
+#define LT_OBJDIR ".libs/"
 
 /* Name of package */
-#undef PACKAGE
+#define PACKAGE "gsl"
 
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
+#define PACKAGE_NAME "gsl"
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+#define PACKAGE_STRING "gsl 2.5"
 
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
+#define PACKAGE_TARNAME "gsl"
 
 /* Define to the home page for this package. */
-#undef PACKAGE_URL
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
+#define PACKAGE_VERSION "2.5"
 
 /* Defined if this is an official release */
-#undef RELEASED
+#define RELEASED /**/
 
 /* Define to 1 if you have the ANSI C header files. */
-#undef STDC_HEADERS
+#define STDC_HEADERS 1
 
 /* Version number of package */
-#undef VERSION
+#define VERSION "2.5"
 
 /* Define to 1 if type `char' is unsigned and you are not using gcc.  */
 #ifndef __CHAR_UNSIGNED__
-# undef __CHAR_UNSIGNED__
+/* # undef __CHAR_UNSIGNED__ */
 #endif
 
 /* Define to `__inline__' or `__inline' if that's what the C compiler
    calls it, or to nothing if 'inline' is not supported under any name.  */
 #ifndef __cplusplus
-#undef inline
+/* #undef inline */
 #endif
 
 /* Define to `unsigned int' if <sys/types.h> does not define. */
-#undef size_t
+/* #undef size_t */
 
 /* Define to empty if the keyword `volatile' does not work. Warning: valid
    code using `volatile' can become incorrect without. Disable with care. */
-#undef volatile
+/* #undef volatile */
 
 /* Use 0 and 1 for EXIT_SUCCESS and EXIT_FAILURE if we don't have them */
 #if !HAVE_EXIT_SUCCESS_AND_FAILURE
@@ -206,23 +207,23 @@
 #endif
 
 /* Define one of these if you have a known IEEE arithmetic interface */
-#undef HAVE_GNUSPARC_IEEE_INTERFACE
-#undef HAVE_GNUM68K_IEEE_INTERFACE
-#undef HAVE_GNUPPC_IEEE_INTERFACE
-#undef HAVE_GNUX86_IEEE_INTERFACE
-#undef HAVE_SUNOS4_IEEE_INTERFACE
-#undef HAVE_SOLARIS_IEEE_INTERFACE
-#undef HAVE_HPUX11_IEEE_INTERFACE
-#undef HAVE_HPUX_IEEE_INTERFACE
-#undef HAVE_TRU64_IEEE_INTERFACE
-#undef HAVE_IRIX_IEEE_INTERFACE
-#undef HAVE_AIX_IEEE_INTERFACE
-#undef HAVE_FREEBSD_IEEE_INTERFACE
-#undef HAVE_OS2EMX_IEEE_INTERFACE
-#undef HAVE_NETBSD_IEEE_INTERFACE
-#undef HAVE_OPENBSD_IEEE_INTERFACE
-#undef HAVE_DARWIN_IEEE_INTERFACE
-#undef HAVE_DARWIN86_IEEE_INTERFACE
+/* #undef HAVE_GNUSPARC_IEEE_INTERFACE */
+/* #undef HAVE_GNUM68K_IEEE_INTERFACE */
+/* #undef HAVE_GNUPPC_IEEE_INTERFACE */
+#define HAVE_GNUX86_IEEE_INTERFACE 1
+/* #undef HAVE_SUNOS4_IEEE_INTERFACE */
+/* #undef HAVE_SOLARIS_IEEE_INTERFACE */
+/* #undef HAVE_HPUX11_IEEE_INTERFACE */
+/* #undef HAVE_HPUX_IEEE_INTERFACE */
+/* #undef HAVE_TRU64_IEEE_INTERFACE */
+/* #undef HAVE_IRIX_IEEE_INTERFACE */
+/* #undef HAVE_AIX_IEEE_INTERFACE */
+/* #undef HAVE_FREEBSD_IEEE_INTERFACE */
+/* #undef HAVE_OS2EMX_IEEE_INTERFACE */
+/* #undef HAVE_NETBSD_IEEE_INTERFACE */
+/* #undef HAVE_OPENBSD_IEEE_INTERFACE */
+/* #undef HAVE_DARWIN_IEEE_INTERFACE */
+/* #undef HAVE_DARWIN86_IEEE_INTERFACE */
 
 /* Define a rounding function which moves extended precision values
    out of registers and rounds them to double-precision. This should
