4eb8d93790f4847b583adb7fc47f081b9f3e07a9
MacroUniverse <270174408@qq.com>
2019-02-28 23:24:05 -0600

    tested 9j symbol

--- source_only/test.cpp
+++ source_only/test.cpp
@@ -17,13 +17,23 @@ inline std::complex<double> gsl_sf_lngamma_complex(std::complex<double> x)
 int main()
 {
 	using namespace std;
-	int two_ja = 1;
-	int two_jb = 1;
-	int two_jc = 0;
-	int two_ma = 1;
-	int two_mb = -1;
-	int two_mc = 0;
+	int two_ja = 1, two_ma = 1;
+	int two_jb = 1, two_mb = -1;
+	int two_jc = 0, two_mc = 0;
+
+	// should be 1/sqrt(2) = 0.707107...
 	cout << "3j = " << gsl_sf_coupling_3j(two_ja, two_jb, two_jc, two_ma, two_mb, two_mc) << endl << endl;
+
+	int two_jd, two_je, two_jf, two_jg, two_jh, two_ji;
+	// {ja jb jc
+	//  jd je jf
+	//  jg jh ji}
+	two_ja = 6; two_jb = 4, two_jc = 2;
+	two_jd = 8; two_je = 2; two_jf = 4;
+	two_jg = 0; two_jh = 6; two_ji = 4;
+	
+	// should be 0
+	cout << "9j = " << gsl_sf_coupling_9j(two_ja, two_jb, two_jc, two_jd, two_je, two_jf, two_jg, two_jh, two_ji) << endl;
 
 	complex<double> in(1.5, 1.5), out;
 	cout << "gammaC" << in << " = " << exp(gsl_sf_lngamma_complex(in)) << endl;
